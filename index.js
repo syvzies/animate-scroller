e1ae08cc5db6bd2ce23d77bd2abf49e4d97dcae3
Alex Syverson <alex.syverson@gmail.com>
2017-02-28 20:43:44 -0800

    Changing export

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 'use strict';
 
-(function () {
-    var scrollAnimator = {};
 
-    function inViewport (elem) {
+var scrollAnimator = {};
 
-        //Get the bounding rectangle's position in relation to the viewport
-        var elemRect = elem.getBoundingClientRect();
+function inViewport (elem) {
 
-        //Get height of viewport
-        var height = window.innerHeight;
+    //Get the bounding rectangle's position in relation to the viewport
+    var elemRect = elem.getBoundingClientRect();
 
-        //return true if bounding rectangle top or bottom are positive and less than viewport height
-        var topVisible = elemRect.top > 0 && elemRect.top < height;
-        var bottomVisible = elemRect.bottom > 0 && elemRect.bottom < height;
+    //Get height of viewport
+    var height = window.innerHeight;
 
-        return topVisible || bottomVisible;
-    }
+    //return true if bounding rectangle top or bottom are positive and less than viewport height
+    var topVisible = elemRect.top > 0 && elemRect.top < height;
+    var bottomVisible = elemRect.bottom > 0 && elemRect.bottom < height;
 
-    scrollAnimator.animate = function (className) {
-        //add default of anim-elem
-        className = className || 'anim-elem';
-        //get all elements with that class name
-        var elemList = document.getElementsByClassName(className);
+    return topVisible || bottomVisible;
+}
 
-        //You can't use forEach on NodeLists or HTML collections
-        for (var i = 0; i < elemList.length; i++) {
-            var elem = elemList[i];
-            //check if those elements are in view
-            if (inViewport(elem)) {
-                //add class
-                scrollAnimator.addClass(elem, 'anim-trigger');
-            }
+scrollAnimator.animate = function (className) {
+    //add default of anim-elem
+    className = className || 'anim-elem';
+    //get all elements with that class name
+    var elemList = document.getElementsByClassName(className);
+
+    //You can't use forEach on NodeLists or HTML collections
+    for (var i = 0; i < elemList.length; i++) {
+        var elem = elemList[i];
+        //check if those elements are in view
+        if (inViewport(elem)) {
+            //add class
+            scrollAnimator.addClass(elem, 'anim-trigger');
         }
     }
+}
 
-    scrollAnimator.addClass = function (elem, classAdd) {
-        if (elem.className.indexOf(classAdd) < 0) {
-            elem.className = elem.className + ' ' + classAdd;
-        }
+scrollAnimator.addClass = function (elem, classAdd) {
+    if (elem.className.indexOf(classAdd) < 0) {
+        elem.className = elem.className + ' ' + classAdd;
     }
+}
 
-    scrollAnimator.scrollLog = function () {
-        console.log('You made it to the scroller');
-    }
+scrollAnimator.scrollLog = function () {
+    console.log('You made it to the scroller');
+}
 
-    //add event listener
-    scrollAnimator.addScroll = function (id, searchClass) {
-        var scrollContainer = document.getElementById(id);
-        scrollContainer.addEventListener('scroll', function () {
-            scrollAnimator.animate(searchClass);
-        });
-    }
+//add event listener
+scrollAnimator.addScroll = function (id, searchClass) {
+    var scrollContainer = document.getElementById(id);
+    scrollContainer.addEventListener('scroll', function () {
+        scrollAnimator.animate(searchClass);
+    });
+}
 
-    return scrollAnimator;
-})();
+module.exports = scrollAnimator;
